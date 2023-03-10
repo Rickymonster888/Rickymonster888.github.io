11162f82c918b83fa3f3e44872dbeace688f2ccd
Rickymonster888 <sunnnting1242@gmail.com>
2023-03-10 22:44:21 +0800

    1

--- js/main.js
+++ js/main.js
@@ -27,11 +27,12 @@ part5openbutton.addEventListener("click", function () {
 const part0 = document.querySelector(".part0");
 const navorange = document.querySelector(".nav-orange");
 
+const nav = document.querySelector("nav");
 const navfix = (entries, observer) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       navorange.classList.add("fixed");
-      document.querySelector(".nav-white").classList.add("fixed"); //開場動畫觸發後顯示nav並固定
+      document.querySelector(".nav-white").classList.add("fixed2"); //開場動畫觸發後顯示nav並固定
       part0.style = "display:none"; //開場動畫觸發後將隱藏
       document.querySelector(".mobile-nav").classList.add("fixed");
     } else {
@@ -49,11 +50,11 @@ const NavChangecolor = (entries, NavColorObserver) => {
       document.querySelector(".nav-white").classList.remove("appear");
       document.querySelector(".nav-white").classList.add("disappear");
       document.querySelector(".nav-white").classList.add("nav-tab-appear");
-
       navorange.classList.add("appear");
       navorange.classList.remove("disappear");
     } else {
       document.querySelector(".nav-white").classList.add("appear");
+      document.querySelector(".nav-orange").classList.remove("nav-tab-appear");
       document.querySelector(".nav-white").classList.remove("nav-tab-appear");
       document.querySelector(".nav-white").classList.remove("disappear");
       navorange.classList.add("disappear");
@@ -86,8 +87,6 @@ const chagebgtrigger = document.querySelector("#intro-trigger2");
 const part4bgorange = (entries, observer3) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
-      navorange.classList.remove("nav-tab-appear");
-      navwhite.classList.remove("nav-tab-appear");
     } else {
       outerwrapper.classList.remove("no-background");
       outerwrapper.classList.add("orange-background");
@@ -96,12 +95,24 @@ const part4bgorange = (entries, observer3) => {
     }
   });
 };
-
 let observer3 = new IntersectionObserver(part4bgorange);
 observer3.observe(chagebgtrigger);
 
-const dialogmask = document.querySelector(".process-dialog-mask");
+const part4bgorange2 = (entries, observer4) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+    } else {
+      outerwrapper.classList.remove("no-background");
+      outerwrapper.classList.add("orange-background");
+      navorange.classList.add("nav-tab-appear");
+      navwhite.classList.add("nav-tab-appear");
+    }
+  });
+};
+let observer4 = new IntersectionObserver(part4bgorange2);
+observer4.observe(document.getElementById("intro-trigger3"));
 
+const dialogmask = document.querySelector(".process-dialog-mask");
 dialogmask.addEventListener("click", function () {
   dialogmask.classList.remove("dialog-appear");
   document.querySelector(".dialog-lung").className =
@@ -194,3 +205,18 @@ document
       .querySelector(".mobile-navlist")
       .classList.remove("mobile-nav-appear");
   });
+
+const ChangePageStopScroll = (entries, ChangePagetricker) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      document.body.classList.remove("stop-scrolling");
+    } else {
+      document.body.classList.add("stop-scrolling");
+      document.getElementById("fillup-anime").classList.add("spin");
+      document.getElementById("shine").classList.add("shine");
+      document.getElementById("click-me-a").classList.add("change-clickme");
+    }
+  });
+};
+let ChangePagetricker = new IntersectionObserver(ChangePageStopScroll);
+ChangePagetricker.observe(document.getElementById("stop-scroll-trigger"));
